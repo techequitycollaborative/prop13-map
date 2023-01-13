13d2fa569c7084bcf2b60c7d84bed6bf035fb803
Alex Yule <adyule@gmail.com>
2023-01-13 00:35:29 -0800

    cleanup script

--- data/buildTiles.sh
+++ data/buildTiles.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 set -e
 set -u
-# NB! This requires superuser or pg_write_server_files privileges and will write to a file on the server.
-# psql -Atx --echo-all $1 << EOF
-#     COPY (SELECT json_agg(row_to_json(la_manual_est_table))::text FROM la_manual_est_table WHERE address != '' AND lat != '' AND long != ''
-#       ) to 'features.geojson';
-# EOF
 
 # https://stackoverflow.com/questions/13227142/using-row-to-json-with-nested-joins
 SQL="select row_to_json(row) FROM (select *, ST_MakePoint(long::double precision, lat::double precision) as geometry from la_manual_est_table WHERE long <> '' and lat <> '') row;"
